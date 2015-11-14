d07c5078416029dd6806c4c77bc54d333c5e94c3
Runpeng Liu <rliu42@mit.edu>
2015-11-14 02:10:01 -0500

    Camera works

--- server/master_camera.py
+++ server/master_camera.py
@@ -1,3 +1,4 @@
+import cv2
 from cv2 import *
 import numpy as np
 from matplotlib import pyplot as plt
@@ -8,8 +9,8 @@ S_MIN = 0
 S_MAX = 256
 V_MIN = 0
 V_MAX = 256
-FRAME_WIDTH = 1080
-FRAME_HEIGHT = 810
+FRAME_WIDTH = 1920
+FRAME_HEIGHT = 1080
 MAX_NUM_OBJECTS = 10
 MIN_OBJECT_AREA = 20 * 20
 MAX_OBJECT_AREA = FRAME_HEIGHT * FRAME_WIDTH / 2
@@ -20,9 +21,9 @@ windowName3 = "After Morphological Operations"
 trackbarWindowName = "Trackbars"
 
 def on_trackbar():
+	pass
 	# this function gets called 
 	# whenever a trackbar position is changed   
-
 def createTrackbars():
 	namedWindow(trackbarWindowName, 0)
 	createTrackbar("H_MIN", trackbarWindowName, H_MIN, H_MAX, on_trackbar)
@@ -33,6 +34,8 @@ def createTrackbars():
 	createTrackbar("V_MAX", trackbarWindowName, V_MAX, V_MAX, on_trackbar)
 
 def drawObject(x, y, frame):
+	x = int(x)
+	y = int(y)
 	circle(frame, (x, y), 20, (0, 255, 0), 2)
 	if y-25>0:
 		line(frame, (x, y), (x, y-25), (0, 255, 0), 2)
@@ -62,13 +65,10 @@ def morphOps(thresh):
 	dilate(thresh,thresh,dilateElement)
 
 def trackFilteredObject(x, y, threshold, cameraFeed):
-	temp = Mat();
-	threshold.copyTo(temp);
+	temp = threshold
 	#these two vectors needed for output of findContours
-	contours = [[]];
-	hierarchy = [];
 	#find contours of filtered image using openCV findContours function
-	findContours(temp,contours,hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_SIMPLE );
+	image, contours, hierarchy = findContours(temp,3,1);
 	#use moments method to find our filtered object
 	refArea = 0.0;
 	objectFound = False;
@@ -78,41 +78,41 @@ def trackFilteredObject(x, y, threshold, cameraFeed):
 		if numObjects<MAX_NUM_OBJECTS:
 			index = 0;
 			while index >= 0:
-				moment = moments(cv.Mat(contours[index]));
-				area = moment.m00;
+				moment = moments(contours[index]);
+				area = moment["m00"];
 
 				#if the area is less than 20 px by 20px then it is probably just noise
 				#if the area is the same as the 3/2 of the image size, probably just a bad filter
 				#we only want the object with the largest area so we safe a reference area each
 				#iteration and compare it to the area in the next iteration.
 				if area>MIN_OBJECT_AREA and area<MAX_OBJECT_AREA and area>refArea:
-					x = moment.m10/area;
-					y = moment.m01/area;
+					x = moment["m10"]/area;
+					y = moment["m01"]/area;
 					objectFound = True;
 					refArea = area;
 				else:
 					objectFound = False;
-				index = hierarchy[index][0];
+				index = hierarchy[index][0][0];
 			#let user know you found an object
 			if objectFound == True:
-				putText(cameraFeed,"Tracking Object",Point(0,50),2,1,Scalar(0,255,0),2);
-				#draw object location on screen
+				putText(cameraFeed,"Tracking Object",(0,50),2,1,(0,255,0),2);
+					#draw object location on screen
 				drawObject(x,y,cameraFeed);
-		else:
-			putText(cameraFeed,"TOO MUCH NOISE! ADJUST FILTER",Point(0,50),1,2,Scalar(0,0,255),2);
+			else:
+				putText(cameraFeed,"TOO MUCH NOISE! ADJUST FILTER",(0,50),1,2,(0,0,255),2);
 
 
 if __name__ == "__main__":
 	#some boolean variables for different functionality within this
 	#program
-	trackObjects = False;
-	useMorphOps = False;
-	#Matrix to store each frame of the webcam feed
-	cameraFeed = Mat();
+	trackObjects = True;
+	useMorphOps = True;
+	#Matrix to store aech frame of the webcam feed
+	#cameraFeed = np.ndarray((1,1), dtype=np.float32)
 	#matrix storage for HSV image
-	HSV = Mat();
+	#HSV = np.ndarray((1,1), dtype=np.float32)
 	#matrix storage for binary threshold image
-	threshold = Mat();
+	#threshold = np.ndarray((1,1), dtype=np.float32)
 	#x and y values for the location of the object
 	x=0;
 	y=0;
@@ -123,18 +123,18 @@ if __name__ == "__main__":
 	#open capture object at location zero (default location for webcam)
 	capture.open(0);
 	#set height and width of capture frame
-	capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);
-	capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);
+	capture.set(4,FRAME_WIDTH);
+	capture.set(5,FRAME_HEIGHT);
 	#start an infinite loop where webcam feed is copied to cameraFeed matrix
 	#all of our operations will be performed within this loop
 	while True:
 		#store image to matrix
-		capture.read(cameraFeed);
+		rval, cameraFeed = capture.read()
 		#convert frame from BGR to HSV colorspace
-		cvtColor(cameraFeed,HSV,COLOR_BGR2HSV);
+		HSV = cvtColor(cameraFeed,COLOR_BGR2HSV);
 		#filter HSV image between values and store filtered image to
 		#threshold matrix
-		inRange(HSV,Scalar(H_MIN,S_MIN,V_MIN),Scalar(H_MAX,S_MAX,V_MAX),threshold);
+		threshold = inRange(HSV,(H_MIN,S_MIN,V_MIN),(H_MAX,S_MAX,V_MAX));
 		#perform morphological operations on thresholded image to eliminate noise
 		#and emphasize the filtered object(s)
 		if useMorphOps:
